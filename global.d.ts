65449abf4576e7c8689ee4033acd4a740a1eb67e
Mariusz Las <mariusz.las@icloud.com>
2024-09-07 10:19:15 +0100

    add github actions pipeline

--- global.d.ts
+++
@@ -1,8 +0,0 @@
-import en from './messages/en.json';
-
-type Messages = typeof en;
-
-declare global {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface IntlMessages extends Messages {}
-}
